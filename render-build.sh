4a39db8c97375267be6762b321631a28bd2e6025
gosama525us-PHW <gosama525us@gmail.com>
2026-08-20 00:05:44 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pip install -r requirements.txt
+python -m pip install \
+  --retries 10 \
+  --timeout 120 \
+  -r requirements.txt
+
 playwright install --with-deps chromium
 python install_cjk_font.py
